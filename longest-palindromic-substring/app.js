8a12ce012252354b80c0af02e54f407c44914690
xiang.wang <xiang.wang@yintech.cn>
2018-02-23 16:01:14 +0800

    add question 5 desc

--- longest-palindromic-substring/app.js
+++ longest-palindromic-substring/app.js
@@ -1,3 +1,13 @@
+/**
+ * Given a string s, find the longest palindromic substring in s. 
+ * You may assume that the maximum length of s is 1000.
+ * 
+ * Example:
+ * 
+ * Input: "babad"
+ * Output: "bab"
+ * 
+ */
 
 module.exports = longestPalindrome
 module.exports.isPalindrome = isPalindrome
